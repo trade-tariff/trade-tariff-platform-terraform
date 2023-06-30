3ce0c88e5a115f0a0a23f54938e95ea0e7019342
jebw <jeb@jdwilkins.co.uk>
2023-06-30 15:28:49 +0100

    linting

--- cloudfront.tf
+++ cloudfront.tf
@@ -43,7 +43,7 @@ module "cdn" {
       target_origin_id       = "frontend-govpaas-${var.environment}"
       viewer_protocol_policy = "redirect-to-https"
 
-      cache_policy_id          = aws_cloudfront_cache_policy.cache_all_qsa.id
+      cache_policy_id = aws_cloudfront_cache_policy.cache_all_qsa.id
       // origin_request_policy_id = aws_cloudfront_origin_request_policy.forward_all_qsa.id
 
       min_ttl     = 0
