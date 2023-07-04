7bef2be60ce044bef1d9fd9da70436f9ac2fdb28
jebw <jeb@jdwilkins.co.uk>
2023-07-04 17:06:50 +0100

    Revert "Try with one of default policies"
    This reverts commit 7478e6ffa7669cfd8cf47a8ec004c59d0b5635d1.

--- cloudfront.tf
+++ cloudfront.tf
@@ -6,10 +6,6 @@ data "aws_cloudfront_cache_policy" "caching_optimized" {
   name = "Managed-CachingOptimized"
 }
 
-data "aws_cloudfront_origin_request_policy" "all_viewer_except_host_header" {
-  name = "Managed-AllViewerExceptHostHeader"
-}
-
 module "cdn" {
   source = "git@github.com:trade-tariff/trade-tariff-platform-terraform-modules.git//aws/cloudfront?ref=aws/cloudfront-v1.0.1"
 
@@ -48,7 +44,7 @@ module "cdn" {
       viewer_protocol_policy = "redirect-to-https"
 
       cache_policy_id          = aws_cloudfront_cache_policy.cache_all_qsa.id
-      origin_request_policy_id = data.aws_cloudfront_origin_request_policy.all_viewer_except_host_header.id
+      origin_request_policy_id = aws_cloudfront_origin_request_policy.forward_all_qsa.id
 
       min_ttl     = 0
       default_ttl = 86400
